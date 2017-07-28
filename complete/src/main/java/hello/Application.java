0a64f267d2fab424970d9dbc7ef27acc928f76c3
Slobodan Lohja <xpagesbeast@users.noreply.github.com>
2017-07-28 12:16:49 -0500

    Added routes to app java file

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -2,7 +2,10 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -11,7 +14,17 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hi There!. This is Canal Barges first Microservice Service running in Kubernetes on BlueMix. I have two instance me running. I'm exposed via a MicroService.";
+        return "Hi There!. This is CBS Service 1.0 here from BlueMix. I have 2 instance of mine running in this Node. Morning Session";
+    }
+
+    @RequestMapping(value = "/hello/{name}", method = RequestMethod.GET)
+    public String getMovie(@PathVariable String name) {
+        return "Hello " + name + ". Are you checking the new change";
+    }
+
+    @RequestMapping(value = "/check", method = RequestMethod.GET)
+    public String getMovie() {
+        return "Hi There!";
     }
 
     public static void main(String[] args) {
