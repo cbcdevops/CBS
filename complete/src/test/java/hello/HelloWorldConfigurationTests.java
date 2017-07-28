b6e44f3787d04afc64a180e537ebba0e6a684f2b
Slobodan Lohja <xpagesbeast@users.noreply.github.com>
2017-07-28 12:09:00 -0500

    Added Lokesh jUnit test example

--- complete/src/test/java/hello/HelloWorldConfigurationTests.java
+++ complete/src/test/java/hello/HelloWorldConfigurationTests.java
@@ -15,19 +15,28 @@
  */
 package hello;
 
-import static org.junit.Assert.assertEquals;
-
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -40,11 +49,34 @@ public class HelloWorldConfigurationTests {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    static WebDriver driver;
+
+    private MockMvc mockMvc;
+
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
     @Test
-    public void testGreeting() throws Exception {
-        ResponseEntity<String> entity = restTemplate
-                .getForEntity("http://localhost:" + this.port + "/", String.class);
-        assertEquals(HttpStatus.OK, entity.getStatusCode());
+    public void seleniumTest() throws InterruptedException {
+        driver = new HtmlUnitDriver();
+        driver.get("http://localhost:" + this.port + "/check/");
+        String PageTitle = driver.getTitle();
+        String PageMessage = driver.findElement(By.cssSelector("body")).getText();
+        Assert.assertEquals(PageMessage, "Hi There!");
+    }
+
+    @Test
+    public void testEmployee() throws Exception {
+        mockMvc.perform(get("/check/")).andExpect(status().isOk())
+                .andExpect(content().string("Hi There!"));
+
+
     }
 
 }
